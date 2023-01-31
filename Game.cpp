a9ee1dd67771e8c969718fcdfca08782e1c2a9d5
Deimos <deimoshall@gmail.com>
2023-01-31 08:41:40 -0600

    test: create and sunken two ships

--- Game.cpp
+++ Game.cpp
@@ -16,17 +16,24 @@ void Game::start() {
 void Game::setup() {
   player1.createShip(5, 8, 8, 8);
   player1.createShip(1, 6, 2, 6);
+  Serial.println("Ship 1 size: " + String(player1.getShip(0).getSize()));
+  Serial.println("Ship 1 size: " + String(player1.getShip(1).getSize()));
+  Serial.println("Ship 1 size: " + String(player1.getShip(2).getSize()));  
 
   player1.hit(1, 6);
   Serial.println("Destroyer hits: " + String(player1.getShip(1).getHits()));
   player1.hit(2, 6);
-  Serial.println("Destroyer hits: " + String(player1.getShip(2).getHits()));
+  Serial.println("Destroyer hits: " + String(player1.getShip(1).getHits()));
   Serial.println("Sunken ships: " + String(player1.getSunkenShips()));
 
   player1.hit(5, 8);
+  Serial.println("Submarine hits: " + String(player1.getShip(0).getHits()));
   player1.hit(6, 8);
+  Serial.println("Submarine hits: " + String(player1.getShip(0).getHits()));
   player1.hit(7, 8);
+  Serial.println("Submarine hits: " + String(player1.getShip(0).getHits()));
   player1.hit(8, 8);
+  Serial.println("Submarine hits: " + String(player1.getShip(0).getHits()));
   Serial.println("Sunken ships: " + String(player1.getSunkenShips()));
 
   player1.printBoard();
