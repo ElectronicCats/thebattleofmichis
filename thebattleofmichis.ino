ec48a63e401941ddcc6b7e3d1eea0fa831b18b9e
Deimos <deimoshall@gmail.com>
2023-03-06 16:08:37 -0600

    feat: add the espnow example to the main ino file

--- thebattleofmichis.ino
+++ thebattleofmichis.ino
@@ -6,26 +6,101 @@
 Player player;
 int Vertical = Board::Vertical, Horizontal = Board::Horizontal;
 
-typedef struct message {
-  uint8_t x;
-  uint8_t y;
-  uint8_t isHit;
-} message;
+// The boards can stablish communication if they have the same MAC address
+uint8_t newMacAddress[6] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
+
+// Define variables to be sent
+uint8_t x;
+uint8_t y;
+uint8_t color;
+
+// Define variables to store incoming data
+uint8_t incoming_x;
+uint8_t incoming_y;
+uint8_t incoming_color;
+
+typedef struct struct_message {
+    uint8_t x_p;
+    uint8_t y_p;
+    uint8_t color_p;
+} struct_message;
+
+// Create a struct_message called outgoing
+struct_message outgoing;
+
+// Create a struct_message to hold incoming
+struct_message incoming;
+
+esp_now_peer_info_t peerInfo;
+
+// Variable to store if sending data was successful
+String success;
 
 void setup() {
   Serial.begin(115200);
-  setupShips();
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Show original MAC address
+  Serial.println("[OLD] ESP32 Board MAC Address:  " + String(WiFi.macAddress()));
+
+  // Set new MAC address for player 1
+  esp_wifi_set_mac(WIFI_IF_STA, &newMacAddress[0]);
+  Serial.println("[NEW] ESP32 Board MAC Address:  " + String(WiFi.macAddress()));
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, newMacAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  // Register for a callback function that will be called when data is received
+  esp_now_register_recv_cb(OnDataRecv);
+
+  // setupShips();
 }
 
 void loop() {
-  player.loop();
-  player.setCursor('e', 4, 4, 1, Horizontal);
+  // Set values to send
+  outgoing.x_p = 2;
+  outgoing.y_p = 2;
+  outgoing.color_p = 255;
 
-  if (player.button.isPressed()) {
-    Serial.println("x: " + String(player.getCursorX()) + " y: " + String(player.getCursorY()));
-    Serial.println("Is hit: " + String(player.hit(player.getCursorX(), player.getCursorY())));
-    player.resetColors();
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(newMacAddress, (uint8_t *) &outgoing, sizeof(outgoing));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
   }
+  else {
+    Serial.println("Error sending the data");
+  }
+  updateDisplay();
+  delay(10000);
+
+  // player.loop();
+  // player.setCursor('e', 4, 4, 1, Horizontal);
+
+  // if (player.button.isPressed()) {
+  //   Serial.println("x: " + String(player.getCursorX()) + " y: " + String(player.getCursorY()));
+  //   Serial.println("Is hit: " + String(player.hit(player.getCursorX(), player.getCursorY())));
+  //   player.resetColors();
+  // }
 }
 
 void setupShips() {
@@ -71,4 +146,39 @@ void placeShip(int length) {
       }
     }
   }
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    success = "Delivery Success :)";
+  }
+  else{
+    success = "Delivery Fail :(";
+  }
+}
+
+// Callback when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&incoming, incomingData, sizeof(incoming));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  incoming_x = incoming.x_p;
+  incoming_y = incoming.y_p;
+  incoming_color = incoming.color_p;
+}
+
+void updateDisplay(){
+  // Display Readings in Serial Monitor
+  Serial.println("INCOMING ");
+  Serial.print("x: ");
+  Serial.print(incoming.x_p);
+  Serial.println(", ");
+  Serial.print("y: ");
+  Serial.print(incoming.y_p);
+  Serial.println(", ");
+  Serial.print("color: ");
+  Serial.print(incoming.color_p);
+  Serial.println();
 }
