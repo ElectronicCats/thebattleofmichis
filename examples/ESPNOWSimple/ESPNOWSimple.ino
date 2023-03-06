2f99406ad614b9dce807b1e9677ff7dbe9735a42
Deimos <deimoshall@gmail.com>
2023-03-06 13:40:18 -0600

    feat: set the same mac address to the esp32 boards

--- examples/ESPNOWSimple/ESPNOWSimple.ino
+++ examples/ESPNOWSimple/ESPNOWSimple.ino
@@ -1,12 +1,9 @@
 #include <esp_now.h>
+#include <esp_wifi.h>
 #include <WiFi.h>
 
-// REPLACE WITH THE MAC Address of your receiver 
-//uint8_t broadcastAddress[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
-// First MAC: 08:3A:F2:A9:DE:BC
-uint8_t broadcastAddress[] = {0x08, 0x3A, 0xF2, 0xA9, 0xDE, 0xBC};
-// Second MAC: 40:22:D8:74:25:3C
-//uint8_t broadcastAddress[] = {0x40, 0x22, 0xD8, 0x74, 0x25, 0x3C};
+// The boards can stablish communication if they have the same MAC address
+uint8_t newMacAddress[6] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
 
 // Define variables to be sent
 uint8_t x;
@@ -61,14 +58,17 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
  
 void setup() {
   // Init Serial Monitor
-  Serial.begin(9600);
+  Serial.begin(115200);
  
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Show MAC address
-  Serial.print("Dirección MAC: ");
-  Serial.println(WiFi.macAddress());
+  // Show original MAC address
+  Serial.println("[OLD] ESP32 Board MAC Address:  " + String(WiFi.macAddress()));
+
+  // Set new MAC address for player 1
+  esp_wifi_set_mac(WIFI_IF_STA, &newMacAddress[0]);
+  Serial.println("[NEW] ESP32 Board MAC Address:  " + String(WiFi.macAddress()));
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
@@ -81,7 +81,7 @@ void setup() {
   esp_now_register_send_cb(OnDataSent);
   
   // Register peer
-  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  memcpy(peerInfo.peer_addr, newMacAddress, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
   
@@ -95,14 +95,13 @@ void setup() {
 }
  
 void loop() {
- 
   // Set values to send
   outgoing.x_p = 2;
   outgoing.y_p = 2;
   outgoing.color_p = 127;
 
   // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &outgoing, sizeof(outgoing));
+  esp_err_t result = esp_now_send(newMacAddress, (uint8_t *) &outgoing, sizeof(outgoing));
    
   if (result == ESP_OK) {
     Serial.println("Sent with success");
@@ -115,7 +114,6 @@ void loop() {
 }
 
 void updateDisplay(){
-
   // Display Readings in Serial Monitor
   Serial.println("INCOMING ");
   Serial.print("x: ");
